e253d39505d012bdbad9f2e157f87fc157cf5611
joaovictor1205 <joaovictor.lol12@gmail.com>
2019-12-12 17:05:24 -0300

    :computer:

--- file_discover.py
+++ file_discover.py
@@ -1,22 +1,35 @@
 #!/user/bin/python3.6
+# -*- coding: utf-8 -*-
 
 import os
 
 def discover(initial_path):
-    
-    extensions = [
+
+    extensions = [                                                                              # EXTENSAO DE ARQUIVOS ALVO
         'exe,', 'dll', 'so', 'rpm', 'deb', 'vmlinuz', 'img'                                     # Arquivos do Sitema
-        "jpg", "jpeg", "bmp", "gif", "png","svg", "psd", "raw",                                 # imagens
+        "jpg", "jpeg", "bmp", "gif", "png","svg", "psd", "raw",                                 # Imagens
         "mp3", "mp4", "m4a", "aac", "ogg", "flac", "wav", "wma", "aiff", "ape",                 # Audios
-        "avi","flv","m4v","mkv","mov","mpg","mpeg","wmv","swf","3gp",                           # Vídeos
-        "doc","docx","xls","xlsx","ppt","pptx",                                                 # Microsoft office
-        "odt","odp","ods", "txt","rtf","tex","pdf","epub","md","yml","yaml","json","xml","csv", # dados estruturados e config
-        "db", "sql","dbf","mdb","iso",                                                          # bancos de dados e imagens de disco
-        "html","htm","xhtml","php","asp","aspx","js","jsp","css",                               # tecnologias web
+        "avi","flv","m4v","mkv","mov","mpg","mpeg","wmv","swf","3gp",                           # Videos
+        "doc","docx","xls","xlsx","ppt","pptx",                                                 # Microsoft Office
+        "odt","odp","ods", "txt","rtf","tex","pdf","epub","md","yml","yaml","json","xml","csv", # Dados estruturados e config
+        "db", "sql","dbf","mdb","iso",                                                          # Bancos de dados e imagens de disco
+        "html","htm","xhtml","php","asp","aspx","js","jsp","css",                               # Tecnologias web
         "c","cpp","cxx","h","hpp","hxx",                                                        # Código fonte C e C++
-        "java","class","jar",                                                                   #Códigos fonte Java
+        "java","class","jar",                                                                   # Códigos fonte Java
         "ps","bat","vb",                                                                        # Scripts de sistemas windows
         "awk","sh","cgi","pl","ada","swift",                                                    # Scripts de sistemas unix
         "go","py","pyc","bf","coffee",                                                          # Outros tipos de códigos fonte
         "zip","tar", "tgz","bz2","7z","rar","bak"                                               # Arquivos compactados e Backups
-    ];
+    ]
+
+    for dirpath, dirs, files in os.walk(initial_path):
+        for _file in files:
+            abspath = os.path.abspath(os.path.join(dirpath, _file))
+            ext = abspath.split(".")[-1]
+            if ext in extensions:
+                yield abspath
+
+if __name__ == "__main__":
+    x = discover(os.getcwd())
+    for i in x:
+        print(i)
